d89a614791ed3e7f888caba83d671ebd0ec586d4
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-02-25 14:04:40 -0300

    - put some ideas in the animation, but now I need to think about a solution to the backward animation

--- lib/common_widgets/animated/onboarding_buttons_animated_list.dart
+++ lib/common_widgets/animated/onboarding_buttons_animated_list.dart
@@ -1,19 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class OnboardingButtonsAnimatedList extends ConsumerStatefulWidget {
-  const OnboardingButtonsAnimatedList({super.key});
+class OnboardingButtonsAnimatedList extends StatefulWidget {
+  final int currentIndexOnboardingMessage;
+  final ButtonStyleButton topButton;
+  final ButtonStyleButton middleButton;
+  final ButtonStyleButton lowerButton;
+
+  const OnboardingButtonsAnimatedList({
+    super.key,
+    required this.currentIndexOnboardingMessage,
+    required this.topButton,
+    required this.middleButton,
+    required this.lowerButton,
+  });
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _OnboardingButtonsAnimatedListState();
+  State<StatefulWidget> createState() => _OnboardingButtonsAnimatedListState();
 }
 
 class _OnboardingButtonsAnimatedListState
-    extends ConsumerState<OnboardingButtonsAnimatedList> {
+    extends State<OnboardingButtonsAnimatedList> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 1),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _doubleAnimation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+  );
+  late final Animation<RelativeRect> _rectAnimation = 
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    //TODO
-    return Container();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final buttonHeight = (constraints.maxHeight - 14.0) / 3;
+        return Stack(
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Positioned(top: 0.0, bottom: buttonHeight ,left: 0.0, right: 0.0 , child: widget.topButton,),
+            if (widget.currentIndexOnboardingMessage > 0)
+              FadeTransition(opacity: _doubleAnimation, child: widget.middleButton),
+            PositionedTransition(rect: , child: widget.lowerButton),
+          ],
+        );
+      }
+    );
   }
 }
