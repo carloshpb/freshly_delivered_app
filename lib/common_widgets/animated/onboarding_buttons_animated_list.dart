1ae407c3ad90c05abd4772ba169cfe5d8916e90a
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-02-24 23:34:20 -0300

    - new blank animated list

---
+++ lib/common_widgets/animated/onboarding_buttons_animated_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class OnboardingButtonsAnimatedList extends ConsumerStatefulWidget {
+  const OnboardingButtonsAnimatedList({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _OnboardingButtonsAnimatedListState();
+}
+
+class _OnboardingButtonsAnimatedListState
+    extends ConsumerState<OnboardingButtonsAnimatedList> {
+  @override
+  Widget build(BuildContext context) {
+    //TODO
+    return Container();
+  }
+}
