2c11244ab2815c8c1ab6ec9fb2693d6b038e54c1
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-02-24 23:27:35 -0300

    - bad list of buttons

--- lib/features/authentication/presentation/screens/onboarding_screen.dart
+++ lib/features/authentication/presentation/screens/onboarding_screen.dart
@@ -2,9 +2,11 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:vector_graphics/vector_graphics.dart';
 
+import '../../domain/models/onboarding_message.dart';
 import '../controllers/onboarding_controller.dart';
 
 final _currentCarouselIndex = StateProvider.autoDispose<int>((_) => 0);
@@ -17,30 +19,56 @@ class OnboardingScreen extends ConsumerStatefulWidget {
 }
 
 class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+  final _carouselController = CarouselController();
+
+  late final List<OnboardingMessage> onboardingMessages;
+  late final StateController<int> indexStateController;
+  late final int actualIndex;
+
+  final List<Widget> _buttonList = [
+    ElevatedButton(
+      onPressed: () {},
+      child: const Text("NEXT"),
+    ),
+    ElevatedButton(
+      onPressed: () {},
+      child: const Text("BACK"),
+    ),
+    TextButton(
+      onPressed: () {},
+      child: const Text("Skip"),
+    ),
+  ];
+
+  void nextButton() {
+    if (actualIndex != 2) {
+      indexStateController.state = actualIndex + 1;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    actualIndex = ref.watch(_currentCarouselIndex);
+    indexStateController = ref.read(_currentCarouselIndex.notifier);
+    onboardingMessages =
+        ref.read(onboardingControllerProvider.notifier).onboardingMessages;
+
+    //TODO : Make correct list of buttons for this page and remove the sample code below
+    _list = ListModel<int>(
+      listKey: _listKey,
+      initialItems: <int>[0, 1, 2],
+      removedItemBuilder: _buildRemovedItem,
+    );
+    _nextItem = 3;
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final mediaQuery = MediaQuery.of(context);
-    final carouselController = CarouselController();
-    final onboardingMessages =
-        ref.read(onboardingControllerProvider.notifier).onboardingMessages;
-    final actualIndex = ref.watch(_currentCarouselIndex);
-
-    final GlobalKey<AnimatedListState> _listKey =
-        GlobalKey<AnimatedListState>();
-
-    @override
-    void initState() {
-      super.initState();
-
-      //TODO : Make correct list of buttons for this page and remove the sample code below
-      _list = ListModel<int>(
-        listKey: _listKey,
-        initialItems: <int>[0, 1, 2],
-        removedItemBuilder: _buildRemovedItem,
-      );
-      _nextItem = 3;
-    }
 
     return Padding(
       padding: const EdgeInsets.all(12.0),
@@ -53,7 +81,7 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
                 children: [
                   CarouselSlider.builder(
                     itemCount: onboardingMessages.length,
-                    carouselController: carouselController,
+                    carouselController: _carouselController,
                     itemBuilder: (context, index, _) => Column(
                       children: [
                         SvgPicture(
