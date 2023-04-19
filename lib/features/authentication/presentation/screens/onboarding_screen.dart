553f628f1a2f0075834f5529dbbeb8f7eff0f951
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-04-19 18:37:17 -0300

    - updated packages; - fixed onboard screen sizes

--- lib/features/authentication/presentation/screens/onboarding_screen.dart
+++ lib/features/authentication/presentation/screens/onboarding_screen.dart
@@ -49,7 +49,7 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
   Widget build(BuildContext context) {
     late final Function(int) goToPage;
 
-    final theme = Theme.of(context);
+    // final theme = Theme.of(context);
     final mediaQuery = MediaQuery.of(context);
     final List<OnboardingMessage> onboardingMessages =
         ref.read(onboardingControllerProvider.notifier).onboardingMessages;
@@ -111,25 +111,28 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
 
     final backButton = Consumer(
       builder: (context, ref, child) {
-        return ElevatedButton(
-          onPressed: () {
-            goToPage(ref.read(onboardingControllerProvider) - 1);
-            _animateToNewOnboardMessagePage(
-                ref.read(onboardingControllerProvider));
-          },
-          style: ElevatedButton.styleFrom(
-            minimumSize: const Size.fromHeight(54.0),
-            backgroundColor: const Color.fromRGBO(168, 174, 170, 0.5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(
-                10.0,
+        return Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: ElevatedButton(
+            onPressed: () {
+              goToPage(ref.read(onboardingControllerProvider) - 1);
+              _animateToNewOnboardMessagePage(
+                  ref.read(onboardingControllerProvider));
+            },
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(54.0),
+              backgroundColor: const Color.fromRGBO(168, 174, 170, 0.5),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  10.0,
+                ),
               ),
             ),
-          ),
-          child: const Text(
-            "BACK",
-            style: TextStyle(
-              color: Color.fromRGBO(23, 104, 46, 1.0),
+            child: const Text(
+              "BACK",
+              style: TextStyle(
+                color: Color.fromRGBO(23, 104, 46, 1.0),
+              ),
             ),
           ),
         );
@@ -137,10 +140,10 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
     );
 
     goToPage = (int nextPageIndex) {
-      print("\nGOING TO NEXT PAGE\n");
+      //print("\nGOING TO NEXT PAGE\n");
       final currentIndex = ref.read(onboardingControllerProvider);
-      print("NEXT PAGE: $nextPageIndex");
-      print("CURRENT PAGE: $currentIndex");
+      //print("NEXT PAGE: $nextPageIndex");
+      //print("CURRENT PAGE: $currentIndex");
 
       // Don't do anything at the limit of the message page's list
       if (nextPageIndex >= onboardingMessages.length || nextPageIndex < 0) {
@@ -149,17 +152,17 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
 
       // swiping right
       if (nextPageIndex > currentIndex && currentIndex == 0) {
-        print("HAS SWIPED TO RIGHT!");
+        //print("HAS SWIPED TO RIGHT!");
         lowerButtons.insert(1, backButton);
-        print("CURRENT LOWER BUTTONS AFTER INSERT : ${lowerButtons.length}");
+        //print("CURRENT LOWER BUTTONS AFTER INSERT : ${lowerButtons.length}");
         _listKey.currentState!.insertItem(1);
       }
 
       // swiping left
       else if (nextPageIndex < currentIndex && currentIndex == 1) {
-        print("HAS SWIPED TO LEFT!");
+        //print("HAS SWIPED TO LEFT!");
         lowerButtons.removeAt(1);
-        print("CURRENT LOWER BUTTONS AFTER REMOVE : $lowerButtons");
+        //print("CURRENT LOWER BUTTONS AFTER REMOVE : $lowerButtons");
         _listKey.currentState?.removeItem(
           1,
           (context, animation) => FadeTransition(
@@ -176,61 +179,76 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
 
     final onboardingPages = List.generate(
       3,
-      (index) => Container(
-        constraints: BoxConstraints(
-          minHeight: 203,
-          maxHeight: mediaQuery.size.height * 0.4242,
-        ),
-        padding: const EdgeInsets.symmetric(
-          horizontal: 31.0,
-        ),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 3,
-              child: SvgPicture(
-                AssetBytesLoader(
-                  onboardingMessages[index].imageSvgPath,
+      (index) => LayoutBuilder(
+        builder: (context, constraints) {
+          return Container(
+            constraints: constraints,
+            // constraints: BoxConstraints(
+            //   minHeight: 203,
+            //   maxHeight: mediaQuery.size.height * 0.4242,
+            // ),
+            // padding: const EdgeInsets.symmetric(
+            //   horizontal: 31.0,
+            // ),
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: SvgPicture(
+                    AssetBytesLoader(
+                      onboardingMessages[index].imageSvgPath,
+                    ),
+                    fit: BoxFit.fitHeight,
+                  ),
                 ),
-              ),
-            ),
-            const Spacer(),
-            Flexible(
-              child: Text(
-                onboardingMessages[index].title,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Color.fromRGBO(37, 197, 115, 1.0),
-                  fontWeight: FontWeight.w700,
-                  fontSize: 18.0,
-                  height: 1.5,
+                const Spacer(),
+                Flexible(
+                  child: Text(
+                    onboardingMessages[index].title,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Color.fromRGBO(37, 197, 115, 1.0),
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18.0,
+                      height: 1.5,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Flexible(
-              child: Text(
-                onboardingMessages[index].message,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14.0,
-                  height: 1.5,
+                const SizedBox(
+                  height: 7.0,
                 ),
-              ),
+                Flexible(
+                  child: Text(
+                    onboardingMessages[index].message,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.0,
+                      height: 1.5,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
 
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(12.0),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12.0,
+            vertical: 24.0,
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              const Spacer(),
               Container(
                 constraints: BoxConstraints(
                   minHeight: 203,
@@ -256,20 +274,22 @@ class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
                   dotColor: Color.fromRGBO(23, 104, 46, 1.0),
                 ),
               ),
-              Flexible(
-                child: SizedBox(
-                  height: mediaQuery.size.height * 0.173,
-                  child: AnimatedList(
-                    key: _listKey,
-                    initialItemCount: lowerButtons.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index, animation) {
-                      return FadeTransition(
-                        opacity: animation,
-                        child: lowerButtons[index],
-                      );
-                    },
-                  ),
+              // const SizedBox(
+              //   height: 10.0,
+              // ),
+              const Spacer(),
+              SizedBox(
+                height: mediaQuery.size.height * 0.173,
+                child: AnimatedList(
+                  key: _listKey,
+                  initialItemCount: lowerButtons.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index, animation) {
+                    return FadeTransition(
+                      opacity: animation,
+                      child: lowerButtons[index],
+                    );
+                  },
                 ),
               )
             ],
