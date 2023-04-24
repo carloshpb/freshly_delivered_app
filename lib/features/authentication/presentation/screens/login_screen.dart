26cf8a3af202d54b420aaf1c05562d09c72f7e33
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-04-24 19:34:14 -0300

    - new logo sizes; - constants file; - svg not appearing

--- lib/features/authentication/presentation/screens/login_screen.dart
+++ lib/features/authentication/presentation/screens/login_screen.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vector_graphics/vector_graphics.dart';
+
+import '../../../../utils/constants.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      backgroundColor: Color.fromRGBO(37, 197, 115, 1.0),
-      body: Center(
-        child: Text(
-          "Login Screen",
-          style: TextStyle(
-            color: Colors.white,
-          ),
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(37, 197, 115, 1.0),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // AssetBytesLoader(
+            //   LOGO_PATH_SVG_VEC,
+            // ),
+            SvgPicture.asset(
+              LOGO_PATH_SVG,
+              fit: BoxFit.fitHeight,
+            ),
+            Text(
+              "LOGIN",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.w600,
+              ),
+            )
+          ],
         ),
       ),
     );
