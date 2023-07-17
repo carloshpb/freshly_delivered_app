da792768f8b5058cf9680d27b5c08f3d582daffc
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-07-17 13:18:02 -0300

    created first unit tests; new exception; fixed future type in use case; added mockito lib

---
+++ test/base_mock.dart
@@ -0,0 +1,40 @@
+import 'package:freshly_delivered_app/features/authentication/domain/repositories/authentication_repository.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+@GenerateNiceMocks([
+  MockSpec<Listener>(),
+  MockSpec<AuthenticationRepository>(),
+  // MockSpec<CountryManager>(),
+  // MockSpec<CountryWithPhoneCode>(),
+  // MockSpec<FormatPhoneResult>(),
+  // MockSpec<LibPhonenumberTextFormatter>(),
+  // MockSpec<CountryPhoneMapper>(),
+  // MockSpec<PhoneNumberMapper>(),
+  // MockSpec<CountryPhone>(),
+  // MockSpec<PhoneNumber>(),
+  // MockSpec<IntlPhoneMobileRepository>(),
+  // MockSpec<CountryFlagRepository>(),
+  //MockSpec<PhoneCountryService>(),
+])
+import 'base_mock.mocks.dart';
+
+// class FakeAppLocalizations extends Fake implements AppLocalizations {
+//   @override
+//   String country_not_found(String invalidCode) {
+//     return "Country code ($invalidCode) was not recognized";
+//   }
+
+//   @override
+//   String get invalid_phone_number {
+//     return "The phone number is in an invalid format (E.164 standard).";
+//   }
+// }
+
+// class FakeFlutterLibphonenumber extends Mock implements FlutterLibphonenumber {}
+
+// class FakeCountryManager extends Mock implements CountryManager {}
+
+abstract class Listener<T> {
+  void call(T? previous, T next);
+}
