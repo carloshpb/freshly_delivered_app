3cb935c85537673f5fc464d3b8e3c2d0c3532246
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-07-17 21:23:45 -0300

    added more tests

--- test/features/authentication/application/use_cases/sign_up_email_password_use_case_impl_test.dart
+++ test/features/authentication/application/use_cases/sign_up_email_password_use_case_impl_test.dart
@@ -43,7 +43,7 @@ void main() {
 
       when(mockAuthenticationRepository.createUserWithEmailAndPassword(
               mockEmail, mockCorrectPassword, mockName, mockCorrectPhone))
-          .thenReturn(voidFuture);
+          .thenAnswer((_) => voidFuture);
 
       final container = makeProviderContainer(mockAuthenticationRepository);
 
