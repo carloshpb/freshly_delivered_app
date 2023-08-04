68ceb2575e9bf7f382b21aa5c2f52f84737121a6
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-08-04 11:35:50 -0300

    added favorite icon button to product card

--- lib/common_widgets/product_card.dart
+++ lib/common_widgets/product_card.dart
@@ -13,14 +13,26 @@ class ProductCard extends StatelessWidget {
   final double? _height;
   final double? _width;
   final ProductDto _product;
+  final VoidCallback? _onTap;
+  final VoidCallback? _tapFavorite;
+  final bool? _isFavorite;
 
   final NumberFormat _priceFormat = NumberFormat.simpleCurrency();
 
-  ProductCard(
-      {super.key, double? height, double? width, required ProductDto product})
-      : _height = height,
+  ProductCard({
+    super.key,
+    double? height,
+    double? width,
+    required ProductDto product,
+    VoidCallback? onTap,
+    VoidCallback? tapFavorite,
+    bool? isFavorite,
+  })  : _height = height,
         _width = width,
-        _product = product;
+        _product = product,
+        _onTap = onTap,
+        _tapFavorite = tapFavorite,
+        _isFavorite = isFavorite;
 
   @override
   Widget build(BuildContext context) {
@@ -39,85 +51,108 @@ class ProductCard extends StatelessWidget {
       ),
       child: LayoutBuilder(
         builder: (context, constraints) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          return Stack(
             children: [
-              SizedBox(
-                height: constraints.maxHeight * 0.31788079,
-                child: FittedBox(
-                  fit: BoxFit.contain,
-                  child: CachedNetworkImage(
-                    imageUrl: _product.image,
+              Positioned.fill(
+                child: GestureDetector(
+                  onTap: _onTap,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: constraints.maxHeight * 0.31788079,
+                        child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: CachedNetworkImage(
+                            imageUrl: _product.image,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.079470198675,
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.06622516556,
+                        child: AutoSizeText(
+                          _product.title,
+                          textAlign: TextAlign.center,
+                          maxLines: 1,
+                          style: const TextStyle(
+                            color: CustomColors.specialOrange,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.0198675496688,
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.0927152317881,
+                        child: AutoSizeText(
+                          _product.description,
+                          textAlign: TextAlign.center,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.03973509933774,
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.09933774834,
+                        child: AutoSizeText(
+                          _priceFormat.format(_product.price),
+                          textAlign: TextAlign.center,
+                          maxLines: 1,
+                          style: const TextStyle(
+                            color: CustomColors.specialOrange,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.03973509933774,
+                      ),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.1125827814569,
+                        child: ElevatedButton(
+                          onPressed: () => context.go(
+                            AppRouter.itemDetails.path,
+                            extra: _product,
+                          ),
+                          child: const AutoSizeText(
+                            Strings.addToCard,
+                            textAlign: TextAlign.center,
+                            maxLines: 1,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              SizedBox(
-                height: constraints.maxHeight * 0.079470198675,
-              ),
-              SizedBox(
-                height: constraints.maxHeight * 0.06622516556,
-                child: AutoSizeText(
-                  _product.title,
-                  textAlign: TextAlign.center,
-                  maxLines: 1,
-                  style: const TextStyle(
-                    color: CustomColors.specialOrange,
-                    fontWeight: FontWeight.w400,
+              if (_isFavorite != null)
+                Positioned(
+                  top: 8.0,
+                  right: 7.0,
+                  height: constraints.maxHeight * 0.0866225165,
+                  width: constraints.maxHeight * 0.0993377483,
+                  child: IconButton(
+                    icon: Icon((_isFavorite!)
+                        ? Icons.favorite
+                        : Icons.favorite_outline),
+                    onPressed: _tapFavorite,
+                    color: Colors.red,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: constraints.maxHeight * 0.0198675496688,
-              ),
-              SizedBox(
-                height: constraints.maxHeight * 0.0927152317881,
-                child: AutoSizeText(
-                  _product.description,
-                  textAlign: TextAlign.center,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: constraints.maxHeight * 0.03973509933774,
-              ),
-              SizedBox(
-                height: constraints.maxHeight * 0.09933774834,
-                child: AutoSizeText(
-                  _priceFormat.format(_product.price),
-                  textAlign: TextAlign.center,
-                  maxLines: 1,
-                  style: const TextStyle(
-                    color: CustomColors.specialOrange,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: constraints.maxHeight * 0.03973509933774,
-              ),
-              SizedBox(
-                height: constraints.maxHeight * 0.1125827814569,
-                child: ElevatedButton(
-                  onPressed: () => context.go(
-                    AppRouter.itemDetails.path,
-                    extra: _product,
-                  ),
-                  child: const AutoSizeText(
-                    Strings.addToCard,
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
             ],
           );
         },
