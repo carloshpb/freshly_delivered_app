b0cfc535e30c46d890e2e2529243d5a19f01fedb
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-08-22 19:31:41 -0300

    making better firestore_api and cart_repo

--- lib/features/dashboard/data/repositories/cart_remote_repository_impl.dart
+++ lib/features/dashboard/data/repositories/cart_remote_repository_impl.dart
@@ -2,35 +2,101 @@ import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../domain/models/cart.dart';
-import '../../domain/models/product.dart';
+import '../../../../constants/strings.dart';
+import '../../domain/models/cart_item.dart';
 import '../../domain/repositories/cart_repository.dart';
 import '../apis/remote/firestore_api.dart';
 import '../apis/remote/impl/firestore_api_impl.dart';
 
-// final cartRemoteRepositoryProvider = Provider<CartRepository>(
-//   (ref) => CartRemoteRepositoryImpl(ref.watch(sqliteApiProvider)),
-// );
+// import '../../../../utils/update_map_helper.dart' as update_map_helper;
 
-final cartRemoteRepositoryProvider = Provider<CartRepository>(
-  (ref) => CartRemoteRepositoryImpl(ref.watch(firestoreApiProvider)),
+final cartRemoteRepositoryProvider = Provider.autoDispose<CartRepository>(
+  (ref) {
+    var firestoreApi = ref.watch(firestoreApiProvider);
+    var cartStreamController = StreamController<List<CartItem>>.broadcast();
+
+    firestoreApi.findAll(Strings.userCartRemoteTable).then((listCartItemsJson) {
+      var cartItemsModels = (listCartItemsJson.isNotEmpty)
+          ? listCartItemsJson.map((ci) => CartItem.fromJson(ci)).toList()
+          : <CartItem>[];
+      cartStreamController.add(cartItemsModels);
+    });
+
+    ref.onDispose(
+      () => cartStreamController.close(),
+    );
+
+    return CartRemoteRepositoryImpl(firestoreApi, cartStreamController);
+  },
 );
 
 class CartRemoteRepositoryImpl implements CartRepository {
   final FirestoreApi _firestoreApi;
+  final StreamController<List<CartItem>> _cartItemsStreamController;
 
-  CartRemoteRepositoryImpl(FirestoreApi firestoreApi)
-      : _firestoreApi = firestoreApi;
+  CartRemoteRepositoryImpl(
+    FirestoreApi firestoreApi,
+    StreamController<List<CartItem>> cartItemsStreamController,
+  )   : _firestoreApi = firestoreApi,
+        _cartItemsStreamController = cartItemsStreamController;
 
   @override
-  Future<void> addProductToCart(Product product, int amount) {
-    // TODO: implement addProductToCart
-    throw UnimplementedError();
+  Future<void> setProductToCart(CartItem item) async {
+    var itemJson = item.toJson();
+    try {
+      await _firestoreApi.save(
+        Strings.userCartRemoteTable,
+        itemJson,
+      );
+    } on Exception {
+      // TODO : Handle errors
+      return;
+    }
   }
 
   @override
-  FutureOr<Stream<Cart>> fetchCartById(String currentUserId) {
-    // TODO: implement fetchCartById
+  Stream<List<CartItem>> fetchCartProducts() {
+    return _cartItemsStreamController.stream;
+  }
+
+  @override
+  Future<void> removeProductAtCart(CartItem item) {
+    // TODO: implement removeProductAtCart
     throw UnimplementedError();
   }
+
+  //   var lastCartItemList = await _cartItemsStreamController.stream.last;
+  //   lastCartItemList[lastCartItemList.indexWhere(
+  //           (currentCartItem) => currentCartItem.productId == item.productId)] =
+  //       item;
+  //   _cartItemsStreamController.add(lastCartItemList);
+
+  //   // var resultCartItem = await _firestoreApi.findById(
+  //   //   Strings.userCartRemoteTable,
+  //   //   item.productId,
+  //   // );
+  //   // int result = -1;
+  //   if (resultCartItem.isNotEmpty && resultCartItem["amount"] != item.amount) {
+  //     result = await _firestoreApi.update(
+  //       Strings.userCartRemoteTable,
+  //       [(attributeName: "amount", value: item.amount)],
+  //       (attributeName: "product_id", equalValue: item.productId),
+  //     );
+  //   } else if (resultCartItem.isEmpty) {
+  //     var itemJson = item.toJson();
+  //     result = await _firestoreApi.save(
+  //       Strings.userCartRemoteTable,
+  //       itemJson,
+  //       itemJson.keys.toList(),
+  //     );
+  //   } else {
+  //     // Do nothing
+  //   }
+  //   if (result > 0) {
+  //     var lastCartItemList = await _cartItemsStreamController.stream.last;
+  //     lastCartItemList[lastCartItemList.indexWhere((currentCartItem) =>
+  //         currentCartItem.productId == item.productId)] = item;
+  //     _cartItemsStreamController.add(lastCartItemList);
+  //   }
+  // }
 }
--- lib/features/dashboard/domain/repositories/cart_repository.dart
+++ lib/features/dashboard/domain/repositories/cart_repository.dart
@@ -4,5 +4,7 @@ import '../models/cart_item.dart';
 
 abstract class CartRepository {
   Stream<List<CartItem>> fetchCartProducts();
-  Future<void> addProductToCart(CartItem item);
+  Future<void> setProductToCart(CartItem item);
+  // Future<void> updateProductAtCart(CartItem item);
+  Future<void> removeProductAtCart(CartItem item);
 }
