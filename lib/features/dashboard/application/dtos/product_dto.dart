bdaa11b6a5746067fb6098437c36956b7c18cf91
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-09-01 17:09:05 -0300

    Dtos now handle better transformation to/from domain

--- lib/features/dashboard/application/dtos/product_dto.dart
+++ lib/features/dashboard/application/dtos/product_dto.dart
@@ -8,15 +8,13 @@ part 'product_dto.freezed.dart';
 sealed class ProductDto with _$ProductDto {
   const ProductDto._();
 
-  const factory ProductDto.empty() = EmptyProductDto;
-
   @Assert('price >= 0.0', 'price cannot be negative')
   @Assert('id != ""', 'id cannot be empty')
   @Assert('title != ""', 'title cannot be empty')
   @Assert('description != ""', 'description cannot be empty')
   @Assert('category != ""', 'category cannot be empty')
   @Assert('imagePath != ""', 'imagePath cannot be empty')
-  const factory ProductDto.normal({
+  const factory ProductDto({
     required String id,
     required String title,
     required double price,
@@ -26,37 +24,29 @@ sealed class ProductDto with _$ProductDto {
     @Default(0) int unitsSold,
     @Default('') String advertisementId,
     @Default(0) int discount,
-  }) = NormalProductDto;
+  }) = _ProductDto;
 
-  factory ProductDto.fromDomain(Product domain) => switch (domain) {
-        NormalProduct() => NormalProductDto(
-            id: domain.id,
-            title: domain.title,
-            price: domain.price,
-            description: domain.description,
-            imagePath: domain.imagePath,
-            category: domain.category,
-            unitsSold: domain.unitsSold,
-            advertisementId: domain.advertisementId,
-            discount: domain.discount,
-          ),
-        _ => const EmptyProductDto(),
-      };
+  factory ProductDto.fromDomain(Product domain) => ProductDto(
+        id: domain.id,
+        title: domain.title,
+        price: domain.price,
+        description: domain.description,
+        imagePath: domain.imagePath,
+        category: domain.category,
+        unitsSold: domain.unitsSold,
+        advertisementId: domain.advertisementId,
+        discount: domain.discount,
+      );
 
-  Product toDomain() {
-    return switch (this) {
-      NormalProductDto() => NormalProduct(
-          id: (this as NormalProductDto).id,
-          title: (this as NormalProductDto).title,
-          price: (this as NormalProductDto).price,
-          description: (this as NormalProductDto).description,
-          imagePath: (this as NormalProductDto).imagePath,
-          category: (this as NormalProductDto).category,
-          unitsSold: (this as NormalProductDto).unitsSold,
-          advertisementId: (this as NormalProductDto).advertisementId,
-          discount: (this as NormalProductDto).discount,
-        ),
-      _ => const EmptyProduct(),
-    };
-  }
+  Product toDomain() => Product(
+        id: id,
+        title: title,
+        price: price,
+        description: description,
+        imagePath: imagePath,
+        category: category,
+        unitsSold: unitsSold,
+        advertisementId: advertisementId,
+        discount: discount,
+      );
 }
