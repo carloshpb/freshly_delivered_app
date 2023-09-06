5eb969f771c34032197296a32f12d04576dc41e9
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-09-06 12:38:57 -0300

    small fix on advertisement model

--- lib/features/dashboard/domain/models/advertisement.dart
+++ lib/features/dashboard/domain/models/advertisement.dart
@@ -11,11 +11,6 @@ part 'advertisement.g.dart';
 
 @freezed
 class Advertisement with _$Advertisement {
-  // const Advertisement._();
-
-  // @JsonSerializable(explicitToJson: true)
-  // const factory Advertisement.empty() = EmptyAdvertisement;
-
   @JsonSerializable(explicitToJson: true)
   @Assert('id != ""', 'id cannot be empty')
   @Assert('description != ""', 'description cannot be empty')
@@ -36,53 +31,4 @@ class Advertisement with _$Advertisement {
 
   factory Advertisement.fromJson(Map<String, Object?> json) =>
       _$AdvertisementFromJson(json);
-
-  // factory Advertisement.fromSqliteJson(Map<String, Object?> json) {
-  //   var datetimeConverter = const DateTimeToMillisecondsSinceEpochConverter();
-
-  //   switch (json['runtimeType']) {
-  //     case 'empty':
-  //       return EmptyAdvertisement.fromJson(json);
-  //     case 'normal':
-  //       return NormalAdvertisement(
-  //         id: json['id'] as String,
-  //         description: json['description'] as String,
-  //         imagePath: json['image_path'] as String,
-  //         isSpecial: json['is_special'] as bool,
-  //         discount: json['discount'] as int,
-  //         createdAt: datetimeConverter.fromJson(json['created_at'] as int),
-  //         modifiedAt: datetimeConverter.fromJson(json['modified_at'] as int),
-  //       );
-
-  //     default:
-  //       throw CheckedFromJsonException(json, 'runtimeType', 'Advertisement',
-  //           'Invalid union type "${json['runtimeType']}"!');
-  //   }
-  // }
-
-  // Map<String, dynamic> toSqliteJson() {
-  //   var datetimeConverter = const DateTimeToMillisecondsSinceEpochConverter();
-
-  //   switch (this) {
-  //     case EmptyAdvertisement():
-  //       return toJson();
-  //     case NormalAdvertisement():
-  //       return <String, dynamic>{
-  //         'id': (this as NormalAdvertisement).id,
-  //         'description': (this as NormalAdvertisement).description,
-  //         'image_path': (this as NormalAdvertisement).imagePath,
-  //         'is_special': (this as NormalAdvertisement).isSpecial,
-  //         'discount': (this as NormalAdvertisement).discount,
-  //         'created_at':
-  //             datetimeConverter.toJson((this as NormalAdvertisement).createdAt),
-  //         'modified_at': datetimeConverter
-  //             .toJson((this as NormalAdvertisement).modifiedAt),
-  //         'runtimeType': "normal",
-  //       };
-  //     default:
-  //       var thisJson = toJson();
-  //       throw CheckedToJsonException(thisJson, 'runtimeType', 'Advertisement',
-  //           'Invalid union type "${thisJson['runtimeType']}"!');
-  //   }
-  // }
 }
