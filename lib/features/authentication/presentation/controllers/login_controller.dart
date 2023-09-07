43d978891d5e1af10060b29883f33231b5c7ec9f
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-09-07 15:11:16 -0300

    fixed listener data type to be the same from login_controller; moved navigation from controller to screen; better state management in login_screen and controller

--- lib/features/authentication/presentation/controllers/login_controller.dart
+++ lib/features/authentication/presentation/controllers/login_controller.dart
@@ -2,37 +2,63 @@ import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../../routers/app_router.dart';
 import '../../application/use_cases/sign_in_email_password_use_case_impl.dart';
 
-final loginControllerProvider =
-    AsyncNotifierProvider.autoDispose<LoginController, (String, String, bool)>(
+final loginControllerProvider = AsyncNotifierProvider.autoDispose<
+    LoginController,
+    ({
+      String email,
+      String password,
+      bool loginSuccess,
+    })>(
   () => LoginController(),
   name: r'loginControllerProvider',
 );
 
-class LoginController extends AutoDisposeAsyncNotifier<(String, String, bool)> {
+class LoginController extends AutoDisposeAsyncNotifier<
+    ({
+      String email,
+      String password,
+      bool loginSuccess,
+    })> {
   @override
-  FutureOr<(String, String, bool)> build() {
-    return ('', '', false);
+  FutureOr<
+      ({
+        String email,
+        String password,
+        bool loginSuccess,
+      })> build() {
+    return (
+      email: '',
+      password: '',
+      loginSuccess: false,
+    );
   }
 
   void updateEmail(String email) {
-    state = AsyncValue.data((email, state.value!.$2, state.value!.$3));
+    state = AsyncValue.data((
+      email: email,
+      password: state.value!.password,
+      loginSuccess: state.value!.loginSuccess,
+    ));
   }
 
   void updatePassword(String password) {
-    state = AsyncValue.data((state.value!.$1, password, state.value!.$3));
+    state = AsyncValue.data((
+      email: state.value!.email,
+      password: password,
+      loginSuccess: state.value!.loginSuccess,
+    ));
   }
 
   Future<void> signIn() async {
-    var email = state.value!.$1;
-    var password = state.value!.$2;
+    var email = state.value!.email;
+    var password = state.value!.password;
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
       await ref.watch(signInUseCaseProvider).execute((email, password));
-      ref.watch(goRouterProvider).go(AppRouter.home.path);
-      return (email, password, true);
+      // ref.watch(goRouterProvider).go(AppRouter.home.path);
+      return (email: email, password: password, loginSuccess: true);
     });
   }
 }
