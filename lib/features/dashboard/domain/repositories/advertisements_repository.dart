02453f1b9aaf98a8fc1427543897f1ea85d01ed9
Carlos Humberto de Paula Borges <carloshumberto1990@gmail.com>
2023-09-11 13:15:40 -0300

    updated better arguments to repositories methods of products and advertisements

--- lib/features/dashboard/domain/repositories/advertisements_repository.dart
+++ lib/features/dashboard/domain/repositories/advertisements_repository.dart
@@ -4,11 +4,21 @@ import '../models/advertisement.dart';
 
 abstract class AdvertisementsRepository {
   FutureOr<Advertisement> findAdvertisementById(String id);
-  FutureOr<List<Advertisement>> findAdvertisementsWithLimit(int limit,
-      ({Advertisement? advertisementObject, int position}) lastAdvertisement);
+  FutureOr<List<Advertisement>> findAdvertisementsWithLimit({
+    required ({
+      Advertisement? advertisementObject,
+      int position,
+    }) lastAdvertisement,
+    int expirationLimitMinutes = 10,
+  });
   FutureOr<List<Advertisement>> findAllAdvertisements();
-  FutureOr<List<Advertisement>> findSpecialAdvertisements(int limit,
-      ({Advertisement? advertisementObject, int position}) lastAdvertisement);
+  FutureOr<List<Advertisement>> findSpecialAdvertisements({
+    required ({
+      Advertisement? advertisementObject,
+      int position,
+    }) lastAdvertisement,
+    int expirationLimitMinutes = 10,
+  });
   Future<void> saveAdvertisements(List<Advertisement> advertisements);
   Future<void> insertOrReplaceAdvertisements(
       List<Advertisement> advertisements);
